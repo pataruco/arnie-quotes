2a3c2ccfd2dac5e4b55abb1207a10e648307ca92
Pedro Martin <PedroM@spacebarmedia.com>
2017-08-18 16:19:29 +0100

    MVP done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
-
 const contentful = require('contentful');
+const Vue = require('vue');
 const SPACE_ID = '6t8um8649ku7';
 const ACCESS_TOKEN = 'b1f8e5581b04e86e16d6ca876d41904c2c9f026c1ec806f246013e134a464c32';
 const SPACE_NAME = 'vue-sandbox';
@@ -10,7 +10,34 @@ const client = contentful.createClient({
      // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
      accessToken: ACCESS_TOKEN
 })
-// This API call will request an entry with the specified ID from the space defined at the top, using a space-specific access token.
-client.getEntries()
-     .then((response) => console.log(response.items))
-     .catch(console.error)
+
+window.onload = function ( ) {
+     let quote = new Vue({
+          el: '#app',
+          data: {
+               quotes: [ ],
+               quote: { }
+          },
+          methods: {
+               getQuotes: function( ) {
+                    client.getEntries()
+                    .then( (response) => {
+                         this.quotes = response.items;
+                         this.getRandomQuote()
+                    })
+                    .catch(console.error)
+               },
+               getRandomQuote: function( ) {
+                    let quotesLength = this.quotes.length;
+                    let randomNumber = this.getRandomArbitrary( 0, quotesLength );
+                    return this.quote = this.quotes[randomNumber].fields;
+               },
+               getRandomArbitrary(min, max) {
+                    return Math.floor( Math.random() * (max - min) + min );
+               }
+          },
+          beforeMount( ) {
+               this.getQuotes( );
+          }
+     });
+}
