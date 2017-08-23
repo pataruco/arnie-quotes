b3abbc33dbb8fbb71b02d0f04712afdd2664d099
Pedro Martin <pataruco@gmail.com>
2017-08-23 21:19:55 +0100

    //WIP

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,41 +5,54 @@ const ACCESS_TOKEN = 'b1f8e5581b04e86e16d6ca876d41904c2c9f026c1ec806f246013e134a
 const SPACE_NAME = 'vue-sandbox';
 
 const client = contentful.createClient({
-     // This is the space ID. A space is like a project folder in Contentful terms
-     space: SPACE_ID,
-     // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-     accessToken: ACCESS_TOKEN
+    // This is the space ID. A space is like a project folder in Contentful terms
+    space: SPACE_ID,
+    // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+    accessToken: ACCESS_TOKEN
 })
 
 const mixitup = require('mixitup');
+const mixer = mixitup('#js-article');
 
 window.onload = function ( ) {
-     let quote = new Vue({
-          el: '#app',
-          data: {
-               quotes: [ ]
-          },
-          methods: {
-               getQuotes: function( ) {
-                    client.getEntries()
-                    .then( (response) => {
-                         this.quotes = response.items;
-                        //  this.getRandomQuote()
-                        console.log(this.quotes[0].fields);
-                    })
-                    .catch(console.error)
-               }
-            //    getRandomQuote: function( ) {
-            //         let quotesLength = this.quotes.length;
-            //         let randomNumber = this.getRandomArbitrary( 0, quotesLength );
-            //         return this.quote = this.quotes[randomNumber].fields;
-            //    },
-            //    getRandomArbitrary(min, max) {
-            //         return Math.floor( Math.random() * (max - min) + min );
-            //    }
-          },
-          beforeMount( ) {
-               this.getQuotes( );
-          }
-     });
+    let quote = new Vue({
+        el: '#app',
+        data: {
+            quotes: [ ],
+            characters: [ ]
+        },
+        methods: {
+            getQuotes: function( ) {
+                client.getEntries()
+                .then( (response) => {
+                    this.quotes = response.items;
+                    this.setCharacters( this.quotes )
+                })
+                .catch(console.error)
+            },
+            filterBy: function ( quoteCharacter ) {
+                mixer.filter( `.${quoteCharacter}` )
+            },
+            setCharacters: function ( quotes ) {
+                let allCharacters = quotes.map( ( quote ) => {
+                    return quote.fields.character;
+                });
+                let selectedCharacters = [ ];
+
+                for ( character of allCharacters ) {
+                    if (  !selectedCharacters.includes( character ) ) {
+                        selectedCharacters.push( character )
+                    }
+                }
+                this.characters = selectedCharacters;
+
+            }
+        },
+        beforeMount( ) {
+            this.getQuotes( );
+        }
+        //   mounted( ) {
+        //
+        //   }
+    });
 }
